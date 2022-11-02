4e8a3a915163f63c28c653fe78164f79ea86cfe8
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-11-02 09:49:41 -0400

    stop building the example project for godot-cpp

--- gdextensions/default.nix
+++ gdextensions/default.nix
@@ -4,43 +4,24 @@
   fetchgit,
   ...
 }: let
-  sconsFlags = "platform=linux target=template_debug";
+  sconsFlags = "platform=linux target=template_debug generate_bindings=yes";
 in
   stdenv.mkDerivation rec {
-    pname = "gdextensions";
-    version = "0.0.1";
-    src = stdenv.mkDerivation rec {
-      name = "godot-cpp";
-      src = fetchgit {
-        url = "https://github.com/godotengine/godot-cpp";
-        rev = "adf4802f4bd7fb7cafc1e94882df3f105d7f7afd";
-        sha256 = "141qjwmbm6a1yl9avalb7l5bp5wcki9jk0lkpryv9pww474ddmcd";
-        fetchSubmodules = true;
-      };
-      buildInputs = [
-        scons
-      ];
-      patches = [
-        ./patches/propagate-env.patch
-      ];
-      buildPhase = ''
-        scons ${sconsFlags}
-      '';
-      installPhase = ''
-        cp -r . $out
-      '';
+    name = "godot-cpp";
+    src = fetchgit {
+      url = "https://github.com/godotengine/godot-cpp";
+      rev = "adf4802f4bd7fb7cafc1e94882df3f105d7f7afd";
+      sha256 = "141qjwmbm6a1yl9avalb7l5bp5wcki9jk0lkpryv9pww474ddmcd";
+      fetchSubmodules = true;
     };
-
     buildInputs = [
       scons
     ];
+    patches = [
+      ./patches/propagate-env.patch
+    ];
     buildPhase = ''
-      pushd test > /dev/null
       scons ${sconsFlags}
-      popd > /dev/null
-    '';
-    patchPhase = ''
-      sed -i "s|res://bin|res://result/bin|g" test/demo/example.gdextension
     '';
     installPhase = ''
       cp -r . $out
